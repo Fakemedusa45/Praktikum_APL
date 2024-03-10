2177485d4bb9e4587422bbde8f9d7bc0846533d7
Fakemedusa45 <161990238+Fakemedusa45@users.noreply.github.com>
2024-03-10 23:19:26 +0800

    Posttest 2

---
+++ Posttest_APL_2/2309106007_RifqiRamadhan_POSTTEST2.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+using namespace std;
+
+#define max 10
+string mobil[max] = {"Ford Capri", "Nissan 300ZX", "Lexus RC", "Toyota 2000GT", "Mercedes-Benz SLR McLaren"};
+string ulang = "y";
+int pos = 5;
+
+void tampilMobil(){
+    system("cls");
+    if (pos > 0){
+        cout << "Daftar Mobil GT\n\n";
+        for (int i = 0; i < pos; i++){
+            cout << i+1 << "." << mobil[i] << "\n";}
+    }else {
+        cout << "data kosong. . ." << endl;
+    }
+        cin.sync();
+    cout << "\n";
+
+}
+
+void hapusMobil(){
+    int del;
+    do{
+        tampilMobil();
+        if(pos > 0){
+            cout << "Hapus data ke : ";
+            cin >> del;
+            for(int i = del; i < pos; i++){
+                mobil[i-1] = mobil[i];
+            }pos--;
+            tampilMobil();
+        cout << "Apakah anda ingin menghapus mobil lagi? (y/n) : ";
+        cin >> ulang;
+        }else {
+            cout << "data tidak ada, silahkan inputkan n : ";
+            cin >> ulang;
+        }
+    }while (ulang == "y");
+}
+
+void tambahMobil(){
+    do{
+        tampilMobil();
+        if (pos < max){
+            cout << "Tambah Data : ";
+            getline(cin, mobil[pos]);
+            pos++;
+            tampilMobil();
+            cout << "Ingin memasukkan data lagi?(y/n) : ";
+            cin >> ulang;
+            cin.ignore();
+        }else {
+            cout << "Daftar Mobil sudah penuh";
+            cout << "Inputkan n untuk kembali : ";
+            cin >> ulang;
+            cin.ignore();
+        }
+    }while (ulang == "y");
+
+}
+
+void ubahMobil(){
+    int edit;
+    do{
+        tampilMobil();
+        cout << "Ubah data ke : ";
+        cin >> edit;
+        cin.ignore();
+        cout << "Silahkan masukkan data baru : ";
+        getline(cin, mobil[edit-1]);
+        tampilMobil();
+        cout << "ingin merubah data lagi? (y/n) : ";
+        cin >> ulang;
+
+    }while (ulang == "y");
+}
+
+int menu(){
+    int pilihan;
+    string kosong;
+    cin.sync();
+    while (true){
+    system("cls");
+    cout << "Selamat datang di menu utama\n     Mobil-mobil GT\n\n";
+    cout << "1. Tampilkan daftar mobil\n";
+    cout << "2. Tambahkan mobil\n";
+    cout << "3. Hapus mobil\n";
+    cout << "4. Ubah Mobil\n";
+    cout << "0. Keluar\n\n";
+    cout << "Silahkan masukkan pilihan menu anda : ";
+    cin >> pilihan;
+    if (pilihan == 1){
+        do{
+            tampilMobil();
+            cout << "Inputkan n untuk kembali : ";
+            cin >> ulang;
+        }while (ulang == "y");
+
+    }else if (pilihan == 2){
+        tambahMobil();
+
+    }else if (pilihan == 3){
+        hapusMobil();
+
+    }else if (pilihan == 4){
+        ubahMobil();
+
+    }else if (pilihan == 0){
+        break;
+
+    }else {
+        cout << "Input invalid";
+    }
+}
+return 0;
+}
+int main() {
+    string input_nama, input_nim;
+
+    cout << "Silahkan login menggunakan nama dan nim anda\n\n";
+    string nama = "Rifqi Ramadhan";
+    string nim = "2309106007";
+    int coba = 1;
+
+    while (coba <= 3) {
+        cout << "Silahkan masukkan nama anda : ";
+        cin.sync();
+        getline (cin, input_nama);
+        cout << "silahkan masukkan nim anda : ";
+        cin.sync();
+        cin >> input_nim;
+        if (input_nama == nama && input_nim == nim){
+            cout << "login berhasil\n\n";
+            coba = 4;
+            menu();
+        }else if (coba == 3){
+            cout << endl;
+            cout << "anda telah 3X gagal login, silahkan jalakan ulang program";
+            break;
+        }
+        else {
+            system("cls");
+            cout << "username yang anda masukkan salah, percobaan ke " << coba <<"x\n\n";
+            coba += 1;
+        }
+    }
+    return 0;
+}
